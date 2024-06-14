7ce50cb45a9de2ad1cfbf9e069089012690335b8
AndresD <camilodelapena.estudiante@iser.edu.co>
2024-06-14 18:45:50 -0500

    guardar registro

--- parcial3/urls.py
+++ parcial3/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from subastas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.login),
-    path('aceptar/',views.login)
+    path('',views.home ,name='home'),
+    path('login/',views.login, name='login')
 ]
--- subastas/views.py
+++ subastas/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+def home(request):
+    return render (request,'index.html')
 
-def login (request):
-    return render (request,'login.html',{
-        'form' : UserCreationForm
-    })
+def login(request):
+    if request.method == 'GET ':
+        return render(request, 'login.html', {
+            'form': UserCreationForm
+        })
+    else :
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], 
+                password=request.POST['password2'])
+                user.save()
+                return HttpResponse('user created succerssfully')
+            except:
+                return HttpResponse('username already xists')
+        return HttpResponse('password do nor match')
