b6c289f21e8bcf7c5c25fc10963f5721e2948065
g <geisonmendoza.estudiante@iser.edu.co>
2024-06-17 09:54:26 -0500

    login , home

--- subastas/views.py
+++ subastas/views.py
@@ -1,22 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
+
 def home(request):
-    return render (request,'index.html')
+    return render(request, 'index.html')
 
 def login(request):
-    if request.method == 'GET ':
+    if request.method == 'GET':
         return render(request, 'login.html', {
-            'form': UserCreationForm
+            'form': UserCreationForm()
         })
-    else :
-        if request.POST['password1'] == request.POST['password2']:
+    else:
+        try:
+            username = request.POST['username']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+        except MultiValueDictKeyError:
+            return HttpResponse('Please complete all fields.', status=400)
+
+        if password1 == password2:
             try:
-                user = User.objects.create_user(username=request.POST['username'], 
-                password=request.POST['password2'])
+                user = User.objects.create_user(username=username, password=password2)
                 user.save()
-                return HttpResponse('user created succerssfully')
+                return HttpResponse('User created successfully')
             except:
-                return HttpResponse('username already xists')
-        return HttpResponse('password do nor match')
+                return render(request, 'login.html', {
+                'form': UserCreationForm ,
+                "error": 'usuario ya existe'
+                })
+
+
+        else:
+            return render(request, 'login.html', {
+                'form': UserCreationForm ,
+                "error": 'password do not match'
+                })
