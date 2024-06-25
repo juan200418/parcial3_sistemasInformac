e4dfd884ec47c6d3c2d48923bff22943fb3ee05b
juank.jaimes21@gmail.com <you@example.com>
2024-06-25 17:49:51 -0500

    agregar rutas al perfil , volver desde crear

--- subastas/models.py
+++ subastas/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     datecompleted = models.DateTimeField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con el usuario propietario de la tarea
